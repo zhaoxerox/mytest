fc66c59a4fd155bd20ee8e65c68e2732c24cdb4d
zhaoxerox <765860534.com>
2021-06-07 17:39:24 +0800

    first commit

--- view/routes/api.php
+++ view/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,11 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('test', TestController::class);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-
-Route::get('/mytest', function () {
-    return 'Hello World';
 });
